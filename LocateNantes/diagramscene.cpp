a5a89def23b17374a16121d8aac623c641ba421e
Laurent Georget <laurent@lgeorget.eu>
2014-02-20 22:35:14 +0100

    toutes les constructions sont visibles pendant le déplacement de la souris

--- LocateNantes/diagramscene.cpp
+++ LocateNantes/diagramscene.cpp
@@ -103,69 +103,6 @@ void DiagramScene::mousePressEvent(QGraphicsSceneMouseEvent *mouseEvent)
         addItem(escalier);
     }
 
-}
-//! [9]
-
-//! [10]
-void DiagramScene::mouseMoveEvent(QGraphicsSceneMouseEvent *mouseEvent)
-{
-    if (myMode == InsertLine && line != 0) {
-        QLineF newLine(line->line().p1(), mouseEvent->scenePos());
-        line->setLine(newLine);
-    } else if (myMode == MoveItem) {
-        QGraphicsScene::mouseMoveEvent(mouseEvent);
-    }
-
-    if (myMode==AddBatiment){
-        bat->setPen(QPen(Qt::black,3,Qt::SolidLine, Qt::RoundCap,Qt::RoundJoin));
-        bat->setRect(QRectF(QPointF(0,0),mouseEvent->scenePos()-startPoint).normalized());
-        bat->setPos(startPoint);
-    }
-}
-//! [10]
-
-//! [11]
-void DiagramScene::mouseReleaseEvent(QGraphicsSceneMouseEvent *mouseEvent)
-{
-
-
-    if (line != 0 && myMode == InsertLine) {
-        QList<QGraphicsItem *> startItems = items(line->line().p1());
-        if (startItems.count() && startItems.first() == line)
-            startItems.removeFirst();
-        QList<QGraphicsItem *> endItems = items(line->line().p2());
-        if (endItems.count() && endItems.first() == line)
-            endItems.removeFirst();
-
-        removeItem(line);
-        delete line;
-        //! [11] //! [12]
-
-        if (startItems.count() > 0 && endItems.count() > 0 &&
-                startItems.first()->type() == DiagramItem::Type &&
-                endItems.first()->type() == DiagramItem::Type &&
-                startItems.first() != endItems.first()) {
-            DiagramItem *startItem =
-                    qgraphicsitem_cast<DiagramItem *>(startItems.first());
-            DiagramItem *endItem =
-                    qgraphicsitem_cast<DiagramItem *>(endItems.first());
-            Arrow *arrow = new Arrow(startItem, endItem);
-            startItem->addArrow(arrow);
-            endItem->addArrow(arrow);
-            arrow->setZValue(-1000.0);
-            addItem(arrow);
-            arrow->updatePosition();
-        }
-    }
-    //! [12] //! [13]
-
-    /*************************************************************************************************/
-    if (myMode==AddBatiment){
-
-	//validé !
-    }
-
-    /*************************************************************************************************/
     if(myMode == AddCouloir){
         QPointF offx = startPoint;
         QPointF offy = mouseEvent->scenePos();
@@ -183,18 +120,6 @@ void DiagramScene::mouseReleaseEvent(QGraphicsSceneMouseEvent *mouseEvent)
             offx += QPointF(0,40);
             offy += QPointF(0,40);
         }
-
-        /*
-        coulS = new CouloirItem(this);
-        coulI = new CouloirItem(this);
-        coulS->setLine(QLineF(QPointF(0,0),mouseEvent->scenePos()-startPoint));
-        coulI->setLine(QLineF(QPointF(0,0),offy-offx));
-        coulS->setPen(QPen(Qt::blue,5,Qt::SolidLine, Qt::RoundCap,Qt::RoundJoin));
-        coulI->setPen(QPen(Qt::blue,5,Qt::SolidLine, Qt::RoundCap,Qt::RoundJoin));
-        coulS->setPos(startPoint);
-        coulI->setPos(offx);
-        addItem(coulS);
-        addItem(coulI);*/
         coulI = new CouloirItem(this);
         coulI->setLine(QLineF(QPointF(0,0),mouseEvent->scenePos()-startPoint));
         coulI->setPen(QPen(Qt::blue,20,Qt::SolidLine, Qt::RoundCap,Qt::RoundJoin));
@@ -249,6 +174,124 @@ void DiagramScene::mouseReleaseEvent(QGraphicsSceneMouseEvent *mouseEvent)
         cloison->setPos(startPoint);
         addItem(cloison);
     }
+}
+//! [9]
+
+//! [10]
+void DiagramScene::mouseMoveEvent(QGraphicsSceneMouseEvent *mouseEvent)
+{
+    if (myMode == InsertLine && line != 0) {
+        QLineF newLine(line->line().p1(), mouseEvent->scenePos());
+        line->setLine(newLine);
+    } else if (myMode == MoveItem) {
+        QGraphicsScene::mouseMoveEvent(mouseEvent);
+    }
+
+    if (myMode==AddBatiment){
+        bat->setPen(QPen(Qt::black,3,Qt::SolidLine, Qt::RoundCap,Qt::RoundJoin));
+        bat->setRect(QRectF(QPointF(0,0),mouseEvent->scenePos()-startPoint).normalized());
+        bat->setPos(startPoint);
+    }
+    if(myMode == AddCouloir){
+        QPointF offx = startPoint;
+        QPointF offy = mouseEvent->scenePos();
+        int testx = abs(offy.x()-offx.x());
+        int testy = abs(offy.y()-offx.y());
+
+        if( testx <= testy ){
+
+            offx += QPointF(40,0);
+            offy += QPointF(40,0);
+        }
+        else if(testx > testy)
+        {
+
+            offx += QPointF(0,40);
+            offy += QPointF(0,40);
+        }
+        coulI->setLine(QLineF(QPointF(0,0),mouseEvent->scenePos()-startPoint));
+        coulI->setPen(QPen(Qt::blue,20,Qt::SolidLine, Qt::RoundCap,Qt::RoundJoin));
+        coulI->setPos(startPoint);
+    }
+
+    if(myMode == AddPorte){
+
+
+        QPointF offxh = startPoint;
+        QPointF offxv = startPoint;
+        QPointF offyv = mouseEvent->scenePos();
+        QPointF offyh = mouseEvent->scenePos();
+        int testx = abs(mouseEvent->scenePos().x()-startPoint.x());
+        int testy = abs(mouseEvent->scenePos().y()-startPoint.y());
+        int distance = (sqrt((testx*testx)+(testy*testy)))*0.25;
+
+
+        if( testx <= testy ){
+            offxh -= QPoint(distance,0);
+            offxv += QPoint(distance,0);
+            offyh -= QPoint(distance,0);
+            offyv += QPoint(distance,0);
+        }
+        else
+        {
+            offxh -= QPoint(0,distance);
+            offxv += QPoint(0,distance);
+            offyh -= QPoint(0,distance);
+            offyv += QPoint(0,distance);
+        }
+        porteG->setRect(QRectF(QPointF(0,0),QSizeF(10,10)));
+        porteG->setPos(startPoint);
+        porteG->setBrush(QBrush(Qt::green));
+        porteD->setRect(QRectF(QPointF(0,0),QSizeF(10,10)));
+        porteD->setBrush(QBrush(Qt::green));
+        porteD->setPos(mouseEvent->scenePos());
+    }
+
+    if(myMode == AddCloison){
+        cloison->setLine(QLineF(QPointF(0,0),mouseEvent->scenePos()-startPoint));
+        cloison->setPen(QPen(Qt::black,5,Qt::SolidLine, Qt::RoundCap,Qt::RoundJoin));
+        cloison->setPos(startPoint);
+    }
+}
+//! [10]
+
+//! [11]
+void DiagramScene::mouseReleaseEvent(QGraphicsSceneMouseEvent *mouseEvent)
+{
+
+
+    if (line != 0 && myMode == InsertLine) {
+        QList<QGraphicsItem *> startItems = items(line->line().p1());
+        if (startItems.count() && startItems.first() == line)
+            startItems.removeFirst();
+        QList<QGraphicsItem *> endItems = items(line->line().p2());
+        if (endItems.count() && endItems.first() == line)
+            endItems.removeFirst();
+
+        removeItem(line);
+        delete line;
+        //! [11] //! [12]
+
+        if (startItems.count() > 0 && endItems.count() > 0 &&
+                startItems.first()->type() == DiagramItem::Type &&
+                endItems.first()->type() == DiagramItem::Type &&
+                startItems.first() != endItems.first()) {
+            DiagramItem *startItem =
+                    qgraphicsitem_cast<DiagramItem *>(startItems.first());
+            DiagramItem *endItem =
+                    qgraphicsitem_cast<DiagramItem *>(endItems.first());
+            Arrow *arrow = new Arrow(startItem, endItem);
+            startItem->addArrow(arrow);
+            endItem->addArrow(arrow);
+            arrow->setZValue(-1000.0);
+            addItem(arrow);
+            arrow->updatePosition();
+        }
+    }
+    //! [12] //! [13]
+
+    /*************************************************************************************************/
+    // si on est en mode bâtiment, il n'y a rien à faire, sinon, changer de mode
     /***************************************************************************************/
 
 
